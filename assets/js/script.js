75a6f677d9554079081af8a0d9b7b1b6a0cdce27
Nihar Sawant <nihar@fusioncharts.com>
2020-05-11 12:37:47 +0530

    Fixed navigation state issue
    - We are using container options to define which data should swup should replace. This approach helped us fix the active state of the navigation
    - Instead of creating a new instance on every single DOMContentLoaded, we are creating a new instance of Swup only when the JS code is executed for the first time
    
    Related to #6

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 (function () {
+    var swup = new Swup({
+        containers: [
+            "#swup-body",
+            "#swup-navigation-primary",
+            '#swup-navigation-secondary'
+        ],
+        plugins: [new SwupScrollPlugin()]
+    });
+
     document.addEventListener('DOMContentLoaded', function () {
         // Mobile Menu Trigger
         var navBurger = document.getElementsByClassName('nav-burger')[0];
         var body = document.getElementsByTagName('body')[0];
-
-        // Initiate Swup transitions
-        var swup = new Swup({
-            plugins: [new SwupScrollPlugin()]
-        });
 
         navBurger.addEventListener('click', function () {
             if (body.classList.contains('site-head-open')) {
@@ -18,7 +22,7 @@
         });
     });
 
-    document.addEventListener('swup:contentReplaced', function () {
+    swup.on('contentReplaced', function () {
         var body = document.getElementsByTagName('body')[0];
         body.classList.remove('site-head-open');
     });
